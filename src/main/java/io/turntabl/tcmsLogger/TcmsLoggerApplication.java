a6c845f90bc5786407e43d8e2d5fdb2783fe07d6
ankomahene <ankomaheneshadrack@gmail.com>
2019-11-20 19:14:24 +0000

    init

---
+++ src/main/java/io/turntabl/tcmsLogger/TcmsLoggerApplication.java
@@ -0,0 +1,13 @@
+package io.turntabl.tcmsLogger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TcmsLoggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TcmsLoggerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/turntabl/tcmsLogger/TcmsLoggerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.turntabl.tcmsLogger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TcmsLoggerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
